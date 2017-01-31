22a02166700a0dfd3935ad94d145325cae3bc246
Daniel Landry <Daniel Landry>
2017-01-30 23:07:19 -0800

    Signed-off-by: Daniel Landry <Daniel Landry>

--- proj/Project_1/main.cpp
+++ proj/Project_1/main.cpp
@@ -32,21 +32,21 @@ int main(int argc, char** argv) {
            face2; // face2= face of player card 
    unsigned short card1; //the Dealer card corresponding to card/face value
    unsigned short card2; //Random card that corresponds with a face/card value
-   char HorL;
+   char HorL; //higher or lower 
    unsigned short games=1; //place holder for games
-   int pot=2;
+   int pot=2; //pot value displayed
    bool safe=true;
-   float PlrAmt= 50;
-   unsigned short choice;
+   float PlrAmt; //starting value for player
+   unsigned short choice; //switch choice
    int a=1; //counter for games
-   int HorL2; //second chance HorL
+   
    ofstream outFile;
    ifstream inFile;
    inFile.open("Earnings.dat"); //open pot.dat file
    outFile.open("Check.dat");  //open 
    
   //program start
-   cout<<"\t\tThis is the game of High or Low\n\n"<<endl;
+   cout<<"\t\tThis is the game of High or Low\n\n"<<endl; //Heading and intro
    
      
    cout<<"Guess if the next card is Higher or Lower than the card shown."<<endl;
@@ -63,7 +63,8 @@ int main(int argc, char** argv) {
        
    cout<<endl;
    cout<<"Are you ready?"<<endl;
-   cout<<"Press [Enter] to begin"<<endl;
+   
+   cout<<"Press [Enter] to begin"<<endl; //press enter to start
    getline(cin,start);
     
     do{                //start of do while loop
@@ -73,24 +74,23 @@ int main(int argc, char** argv) {
    
    //Generates card 1 **********************************************************
    if (card1>=9){
-      cardVal=10;
        switch(card1) {
-           case 9: {
+           case 9: {  //9= ace, value=14
                face1="Ace";
            cardVal=14;}
            break;
-           case 10: {face1="King";
+           case 10: {face1="King"; //10= King, value=13
            cardVal=13;}
            break;
-           case 11: {face1="Queen";
+           case 11: {face1="Queen"; //11= Queen, value=12
            cardVal=12;
            }
            break;
-           case 12: {face1= "Jack";
+           case 12: {face1= "Jack"; //10= jack, value=11
            cardVal=11;
            }
            break;
-           case 13: {face1="Ten";
+           case 13: {face1="Ten"; //13= ten, value 10
            cardVal=10;
           }
            break;
@@ -98,77 +98,66 @@ int main(int argc, char** argv) {
            cout<<"ERROR\n";
        }    
     }
-   else if (card1==8) {
+   else if (card1==8) { //9, value 9
        cardVal=9;
        face1= "Nine";
    }
-   else if (card1==7) {
+   else if (card1==7) { //7 value= 8
        cardVal=8;
        face1="Eight";
    }
-   else if (card1==6) {
+   else if (card1==6) { //6, value=7
        cardVal=7;
        face1="Seven";
    }
-   else if (card1==5){
+   else if (card1==5){// 5 = value 6
        cardVal=6;
        face1="Six";
    }
-   else if (card1==4){
+   else if (card1==4){ //4 value= 5
        cardVal=5;
        face1="Five";
    }
-   else if (card1==3){
+   else if (card1==3){ //3 val4
        cardVal=4;
        face1="Four";
    }
-   else if(card1==2){
+   else if(card1==2){ //2 val=3
        cardVal=3;
        face1="Three";
    }
    else {
-       cardVal=2;
+       cardVal=2;  //1 val=2
        face1="Two";
    }
 //******************************************************************************
    //High or Low?
    cout<<"******************************************************************\n";
    cout<<"Dealers card: "<<face1<<" with the value of "<<cardVal<<endl;
-   cout<<setprecision(100000000);   
+   cout<<setprecision(100000000);  //set precision
+   
    for (int count=a; count<=games;count++) {
-          cout<<"Game: "<<count<<endl;
+          cout<<"Game: "<<count<<endl; //keep count of games
           }
-            games++;
-            a++;  
+            games++;//increase games
+            a++;   //increase count
+games>10?cout<<"You've played over ten games!!"<<endl:cout<< 
+        "Reach over 10 games!"<<endl;  //Reach over ten games
+        
+        
   inFile>>PlrAmt;
-  cout<<"Pot= $"<<pot<<endl;
+  cout<<"Pot= $"<<pot<<endl; //display pot value
   cout<<endl;
-  cout<<"Player's earnings =$"<<static_cast<float>(PlrAmt)<<endl;   
-  cout<<endl;
+  cout<<"Player's earnings =$"<<static_cast<float>(PlrAmt)<<endl;  
+  cout<<endl;  //display player earnings as float
   cout<<"Guess if the next card is higher or lower than the Dealers.\n";
   cout<<"(type in 'h' for high or 'l' for low)"<<endl;
-  cin>>HorL;
+  cin>>HorL;  //decide high or low
   
-  /*
-  if (HorL != 'h' || HorL != 'l') {
-    safe=true;
-     {
-      cout<<"You've entered an incorrect value."<<endl;
-      cout<<"Enter either 'h' or 'l'."<<endl;
-      cin>>HorL;
-      
-      if (HorL=='h' || HorL=='l'){
-          safe=false;
-      }
-      else 
-          safe=true;
-      } while (safe);
-  }   else 
- */
   
   
   //Generate card 2 ************************************************************
-  
+   //generate second random number with same values as specified for card 1
    if (card2>=9){
      
        switch(card2) {
@@ -238,8 +227,8 @@ int main(int argc, char** argv) {
         cout<<"Your Draw: "<<face2<<" with the value of "<<crdVal2<<endl;
         cout<<endl;
       if (cardVal<crdVal2){
-          cout<<"You win!"<<endl;
-         
+          cout<<"You win!"<<endl; //win if guess high, car high
+         //double, save or cash out
         cout<<"What would you like to do next?(type 1,2,or 3)"<<endl;
         cout<<"1. Double up (you lose pot value from your earnings)"<<endl;
         cout<<"2. Save it (add the amount to your earnings)"<<endl;
@@ -263,19 +252,20 @@ int main(int argc, char** argv) {
                 cout<<"A check has been put in the check file in this programs";
                         cout<<" folder"<<endl;
                 outFile<<PlrAmt<<endl;
+                return 0;
             }
             default:
                 cout<<""<<endl;
         }
          
       }
-      else if (cardVal>crdVal2){
+      else if (cardVal>crdVal2){ //lose guess=high, card=low
           cout<<"You lose!"<<endl;
           PlrAmt=PlrAmt-pot;
           pot=2;
       }
       else {         
-          cout<<"You win!"<<endl;
+          cout<<"You win!"<<endl; //win and quadruple pot if same card value
           cout<<"You got the same card as the dealer."<<endl;
           cout<<"The pot is quadrupled!!\n"<<endl;
         cout<<"What would you like to do next?(type 1,2,or 3)"<<endl;
@@ -285,7 +275,7 @@ int main(int argc, char** argv) {
         cin>>choice;
        
         switch (choice) {
-            
+            //double, save or cash out
             case 1: {
                 pot+= pow(pot,4);
                 cout<<"The pot is now =$"<<pot<<endl;                
@@ -301,6 +291,7 @@ int main(int argc, char** argv) {
                cout<<"A check has been put in the check file in this programs";
                         cout<<" folder"<<endl;
                 outFile<<PlrAmt<<endl;
+                 return 0;
             }
             default:
                 cout<<""<<endl;
@@ -314,9 +305,10 @@ else if (HorL=='l') {
         cout<<"Dealer Draw: "<<face1<<" with the value of "<<cardVal<<endl;
         cout<<"Your Draw: "<<face2<<" with the value of "<<crdVal2<<endl;
         cout<<endl;
-      if (cardVal>crdVal2){
+      if (cardVal>crdVal2){ 
           cout<<"You win!"<<endl;
-          
+          //win if guess low, card=low
+          // double, save, or cash out
         cout<<"What would you like to do next?(type 1,2,or 3)"<<endl;
         cout<<"1. Double up (at the risk of losing more money)"<<endl;
         cout<<"2. Save it (add the amount to your earnings)"<<endl;
@@ -340,6 +332,7 @@ else if (HorL=='l') {
                 cout<<"A check has been put in the check file in this programs";
                         cout<<" folder"<<endl;
                 outFile<<PlrAmt<<endl;
+                 return 0;
             }
             break;
             default:
@@ -347,13 +340,13 @@ else if (HorL=='l') {
             
         }
       }
-      else if (cardVal<crdVal2){
+      else if (cardVal<crdVal2){ //lose if guess low, card high
           cout<<"You lose."<<endl;
           PlrAmt=PlrAmt-pot;
           pot=2;
       }
       else {
-           cout<<"You win!"<<endl;
+           cout<<"You win!"<<endl; //win, card the same value as dealer
          
           cout<<endl;
           cout<<"You got the same card as the dealer."<<endl;
@@ -363,7 +356,7 @@ else if (HorL=='l') {
         cout<<"2. Save it (add the amount to your earnings)"<<endl;
         cout<<"3. Cash out (cash out the printable check)"<<endl;
         cin>>choice;
-      
+      // double, save, cash out
         switch (choice) {              
             case 1: {
                 pot+= pow(pot,4);
@@ -380,6 +373,7 @@ else if (HorL=='l') {
                 cout<<"A check has been put in the check file in this programs";
                         cout<<" folder"<<endl;
                 outFile<<PlrAmt<<endl;
+                 return 0;
             }
             default:
                 cout<<""<<endl;
@@ -388,14 +382,14 @@ else if (HorL=='l') {
   }
 else {
     safe=true;
-     {
+     { //if not 'h or l' Retry
       cout<<"You've entered an incorrect value."<<endl;
       cout<<"Retry!"<<endl;
       safe=false;
      }while (safe);
 }
   }while(PlrAmt>=0); 
-  
+  //game over if player amount get to zero
 
    cout<<"Your out of money!!"<<endl;
    cout<<"GAME OVER"<<endl;    
